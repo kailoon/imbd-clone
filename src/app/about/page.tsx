26ae53279420202b3770d3def07840aa81f02558
kailoon <chankailoon@gmail.com>
2023-02-14 14:39:14 +0800

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,18 @@
+export default function AboutPage() {
+	return (
+		<div className="max-w-6xl mx-auto p-4 space-y-4">
+			<h1 className="text-2xl font-medium text-black">About</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptas iusto
+				nostrum ipsum esse, corrupti soluta blanditiis doloremque, consequuntur
+				accusamus magnam libero repellat odit accusantium recusandae quam ullam
+				cumque id ducimus.
+			</p>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla est, qui
+				repellat tenetur quis laborum cumque consequuntur fuga aut delectus
+				eveniet iste eligendi at ullam, deserunt ad magni fugit laboriosam!
+			</p>
+		</div>
+	)
+}
